e9a6fc99e66578771ab04a8dbd06d7f430250be7
Victor Hugo <victor.couto@ufrpe.br>
2025-06-21 14:15:37 -0300

    adicionando feature de filtro de min e max de páginas

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -81,13 +81,7 @@ class LivroController {
 
   static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const {editora, titulo} = req.query;
-
-      
-
-      const busca = {};
-      if(editora)busca.editora = editora;
-      if(titulo)busca.titulo = {$regex: titulo, $options: "i"}; 
+      const busca = processaBusca(req.query);
       const livrosResultado = await livros.find(busca);
 
       res.status(200).send(livrosResultado);
@@ -98,6 +92,18 @@ class LivroController {
 
 
 
+}
+
+function processaBusca(parametros){
+    const {editora, titulo, minPaginas, maxPaginas} = parametros;
+      
+      const busca = {};
+      if(minPaginas || maxPaginas) busca.numeroPaginas = {};
+      if(editora)busca.editora = editora;
+      if(titulo)busca.titulo = {$regex: titulo, $options: "i"}; 
+      if(minPaginas) busca.numeroPaginas.$gte = minPaginas;
+      if(maxPaginas) busca.numeroPaginas.$lte = maxPaginas;
+      return busca;
 }
 
 export default LivroController
